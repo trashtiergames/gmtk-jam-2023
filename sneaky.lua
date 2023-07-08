f024543932653cf4bead71314a9acd0adb650379
trashtiergames <hello@trashtiergames.com>
2023-07-08 16:15:40 +0300

    Add sneaky footsteps

--- sneaky.lua
+++ sneaky.lua
@@ -41,10 +41,18 @@ function sneaky:update()
     if self.step_timer > self.step_timer_max then
       self.step_timer = 0
       self.step += 1
+      -- end of path
+      if self.step > #self.path then
+        self.state = "gone"
+        sfx(01)
+        points -= 1
+        return
+      end
       local coords = self.path[self.step]
       self.gx, self.gy = coords[1], coords[2]
       self.x = self.gx * 8
       self.y = self.gy * 8
+      self:make_footstep_sfx()
     end
   elseif self.state == "discovered" then
     self.anim_timer += 1/60
@@ -53,6 +61,7 @@ function sneaky:update()
       self.anim_timer = 0
     end
   elseif self.state == "gone" then
+    self.visible = false
   end
 end
 
@@ -63,5 +72,19 @@ function sneaky:draw()
       self.x,
       self.y
     )
+  end
+end
+
+function sneaky:make_footstep_sfx()
+  x_dist = abs(sneaky.gx - player.gx)
+  y_dist = abs(sneaky.gy - player.gy)
+  dist =  x_dist + y_dist
+
+  if dist == 3 then
+    sfx(02)
+  elseif dist == 2 then
+    sfx(03)
+  elseif dist <= 1 then
+    sfx(04)
   end
 end
